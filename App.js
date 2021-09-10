a29fa8a57a0f705bff7cfabbd1ba9b17460300dd
Ranjit Raman Salaboyina <34271854+RanjitRaman@users.noreply.github.com>
2021-09-10 19:16:53 +0530

    Update app.js

--- App.js
+++ App.js
@@ -11,17 +11,14 @@ function checkBirthDateIsLucky() {
     outputMessage.innerText = "Enter details correctly ❌";
   } else {
     var newBirthDate = convertDatetoString(birthDate.value);
-    if ((newBirthDate = "23061997")) {
-      outputMessage.innerText = "Neko birthday daniko lucky numberuu!";
+
+    for (i = 0; i < newBirthDate.length; i++) {
+      sum = sum + Number(newBirthDate[i]);
+    }
+    if (sum % luckyNumber.value == 0) {
+      outputMessage.innerText = "Your birthday is lucky ✨";
     } else {
-      for (i = 0; i < newBirthDate.length; i++) {
-        sum = sum + Number(newBirthDate[i]);
-      }
-      if (sum % luckyNumber.value == 0) {
-        outputMessage.innerText = "Your birthday is lucky ✨";
-      } else {
-        outputMessage.innerText = "Your birthday is not lucky 😐";
-      }
+      outputMessage.innerText = "Your birthday is not lucky 😐";
     }
   }
 }
