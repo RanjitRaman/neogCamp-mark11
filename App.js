d45a74a70a37402609e80a614fb870cda35a4d5b
Ranjit Raman Salaboyina <34271854+RanjitRaman@users.noreply.github.com>
2021-09-10 18:42:33 +0530

    Initial mark11

---
+++ App.js
@@ -0,0 +1,28 @@
+var birthDate = document.querySelector("#birth-date");
+var luckyNumber = document.querySelector("#lucky-number");
+var checkButton = document.querySelector("#check-lucky");
+var outputMessage = document.querySelector("#output-message");
+
+outputMessage.style.display = "none";
+function checkBirthDateIsLucky() {
+  outputMessage.style.display = "block";
+  let sum = 0;
+  if (birthDate.value == 0 || luckyNumber.value == 0) {
+    outputMessage.innerText = "Enter details correctly ❌";
+  } else {
+    var newBirthDate = convertDatetoString(birthDate.value);
+    for (i = 0; i < newBirthDate.length; i++) {
+      sum = sum + Number(newBirthDate[i]);
+    }
+    if (sum % luckyNumber.value == 0) {
+      outputMessage.innerText = "Your birthday is lucky ✨";
+    } else {
+      outputMessage.innerText = "Your birthday is not lucky 😐";
+    }
+  }
+}
+
+function convertDatetoString(DOB) {
+  return DOB.replaceAll("-", "");
+}
+checkButton.addEventListener("click", checkBirthDateIsLucky);
